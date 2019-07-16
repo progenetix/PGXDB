b04f42836b5a510f6d2a7c8eed624b882c870bee
mbaudis <mbaudis@me.com>
2019-07-16 16:29:49 +0200

    more config for stats

--- GEOrawData.pm
+++ GEOrawData.pm
@@ -30,13 +30,13 @@ sub pgxdb_filter_downloads {
 	}
 
 	if ($pgxdb->{parameters}->{filters} =~ /\w\w/) {
-		$pgxdb->{parameters}->{filters}	=~	s/[^\w\,\.\-\:]//g;	
+		$pgxdb->{parameters}->{filters}	=~	s/[^\w\,\.\-\:]//g;
 		foreach my $filter (split(',', $pgxdb->{parameters}->{filters})) {
 			$pgxdb->{downloads} = 	[ grep{ $_->{probefile_ftp} =~ /$filter/i } @{ $pgxdb->{downloads} } ];
 	}}
 
 	return $pgxdb;
-	
+
 }
 
 ################################################################################
@@ -54,31 +54,36 @@ sub pgxdb_download_probefiles {
 
 		my $gseDir  =   $pgxdb->{paths}->{georawdir}.'/'.$gsm_meta->{GSE};
 		my $gsmDir  =   $gseDir.'/'.$gsm_meta->{GSM};
-		
+
 		mkdir $gseDir;
 		mkdir $gsmDir;
 
-		foreach (split('::', $gsm_meta->{probefile_ftp})) {
-			
-			my $probefileLoc 	=		$_;
+		my @ftp_files;
 
-			if ($pgxdb->{parameters}->{probefiles} =~ /../) {
-				if (! grep{ $probefileLoc =~ /$_/i } split(',', $pgxdb->{parameters}->{probefiles})) {
-					next } }
-			
+		if ($pgxdb->{parameters}->{probefiles} =~ /../) {
+			foreach my $ftpF (split('::', $gsm_meta->{probefile_ftp})) {
+				if (grep{ $ftpF =~ /\.$_/i } split(',', $pgxdb->{parameters}->{probefiles})) {
+					push(@ftp_files, $ftpF ) }
+			}
+		} else {
+			@ftp_files		=		split('::', $gsm_meta->{probefile_ftp}) }
+
+		foreach my $uri (@ftp_files) {
+
+			my$probefileLoc		=		$uri;	
 			$probefileLoc	=~	s/^.*?\/([^\/]+?$)/$1/;
-			$probefileLoc	=~	s/\.gz$//i;		
+			$probefileLoc	=~	s/\.gz$//i;
 			$probefileLoc	=		$gsmDir.'/'.$probefileLoc;
 
 			if (-f $probefileLoc) { next }
 
 			print "
-$i / ".@{ $pgxdb->{downloads} }.": Fetching	
+$i / ".@{ $pgxdb->{downloads} }.": Fetching
   $_
   => $probefileLoc\n";
 
-			my $fh        =   File::Fetch->new(uri => $_);
-			my $where     =   $fh->fetch(to => $gsmDir ) or die $ff->error;
+			my $fh        =   File::Fetch->new(uri => $uri);
+			my $where     =   $fh->fetch(to => $gsmDir ) or warn $ff->error;
 		# name of the file downloaded
 			my $gzDlName   	=   $fh->file;
 			my $gzDl				=		$gsmDir.'/'.$gzDlName;
@@ -86,13 +91,11 @@ $i / ".@{ $pgxdb->{downloads} }.": Fetching
 			my $ok          =   $ae->extract( to => $gsmDir) or warn $ae->error;
 			if (-f $probefileLoc) {
 				unlink $gzDl or warn "Could not unlink $gzDl" }
-				
-		}
 
-	}
+	}}
 
 	return $pgxdb;
-	
+
 }
 
 ################################################################################
@@ -100,7 +103,7 @@ $i / ".@{ $pgxdb->{downloads} }.": Fetching
 sub pgxdb_read_GSM_logfile {
 
   my $pgxdb    	=   shift;
-  
+
   $pgxdb->{downloads}	=		[];
 	my @lines;
 	my $fCont 		=   q{};
